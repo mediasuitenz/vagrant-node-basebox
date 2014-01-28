a528177a7859086f43cb5faa3c7517f8e563e844
Richard Walker <digitalsadhu@gmail.com>
2014-01-28 18:43:22 +1300

    Add mysql to node basebox

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -69,3 +69,59 @@ class { 'mongodb': }
 mongodb::user { 'user':
   password => 'password',
 }
+
+class {'mysql::server':
+  root_password => 'password',
+  override_options => {
+    mysqld => {
+      bind_address => '0.0.0.0',
+      "skip-external-locking" => "false",
+    }
+  }
+}
+->
+mysql_user { 'user@localhost':
+  ensure                   => 'present',
+  max_connections_per_hour => '0',
+  max_queries_per_hour     => '0',
+  max_updates_per_hour     => '0',
+  max_user_connections     => '0',
+  password_hash            => '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19',
+}
+->
+mysql_grant { 'user@localhost/*.*':
+  ensure     => 'present',
+  options    => ['GRANT'],
+  privileges => ['ALL'],
+  table      => '*.*',
+  user       => 'user@localhost',
+}
+->
+mysql_grant { 'user@%/*.*':
+  ensure     => 'present',
+  options    => ['GRANT'],
+  privileges => ['ALL'],
+  table      => '*.*',
+  user       => 'user@%',
+}
+
+mysql::db { 'development':
+  user     => 'user',
+  password => 'password',
+  host     => '%',
+  grant    => ['ALL '],
+}
+
+mysql::db { 'production':
+  user     => 'user',
+  password => 'password',
+  host     => '%',
+  grant    => ['ALL'],
+}
+
+mysql::db { 'testing':
+  user     => 'user',
+  password => 'password',
+  host     => '%',
+  grant    => ['ALL'],
+}
